ff881ec97712fa85b064463c3c7edd48b012f118
FFundee <129069776+FFundee@users.noreply.github.com>
2026-01-22 09:05:53 +0300

    Update 105.cpp

---
+++ 105.cpp
@@ -0,0 +1,17 @@
+#include <Windows.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    int meters; 
+    int kilometers; 
+    cout << "Введите расстояние в метрах: ";
+    cin >> meters;
+    kilometers = meters / 1000;
+    cout << "Число полных километров: " << kilometers << endl;
+
+    return 0;
+}
